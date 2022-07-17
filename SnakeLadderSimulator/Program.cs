f41728d22b5be1cc5e7b5a8b40cfe7c7022d75e8
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-17 14:09:10 +0530

    Roll die 1-6 added

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Welcome in Snake & Ladder Simulator.");
 int position = 0;
+Random random = new Random();
+int rolldie = random.Next(1, 7);
 Console.WriteLine("Player 1 position is " + position);
+Console.WriteLine("Roll Die value: "+rolldie);
